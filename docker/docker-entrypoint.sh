af442d2393c0fb9e055b91edb721cba2e5dee3ab
Stafford Williams <stafford.williams@gmail.com>
2025-03-26 22:58:17 +1100

    env vars

--- docker/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Replace ServerName in both config files
-sed -i "s/ServerName .*/ServerName ${SERVER_NAME}/" /usr/local/apache2/conf/httpd.conf
-sed -i "s/ServerName .*/ServerName ${SERVER_NAME}/" /usr/local/apache2/conf/extra/httpd-ssl.conf
-
-# Start Apache in foreground
-exec httpd-foreground
