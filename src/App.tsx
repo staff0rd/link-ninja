7bcd20252254c8dcc30c505b7dc94b73f081f56e
Stafford Williams <stafford.williams@gmail.com>
2026-01-18 21:48:15 +1100

    migrate from eslint to biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, createTheme, ThemeProvider } from "@mui/material";
 import { useAtomValue } from "jotai";
 import { authAtom } from "./atoms";
 import Form from "./Form";
 import Login from "./Login";
 
 const theme = createTheme({
-  palette: { mode: "dark" },
+	palette: { mode: "dark" },
 });
 
 function App() {
-  const isAuthenticated = useAtomValue(authAtom);
+	const isAuthenticated = useAtomValue(authAtom);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {isAuthenticated ? (
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            height: "100%",
-            margin: "0 auto",
-            padding: 2,
-            alignItems: "center",
-          }}
-        >
-          <Form />
-        </Box>
-      ) : (
-        <Login />
-      )}
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			{isAuthenticated ? (
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						width: "100%",
+						height: "100%",
+						margin: "0 auto",
+						padding: 2,
+						alignItems: "center",
+					}}
+				>
+					<Form />
+				</Box>
+			) : (
+				<Login />
+			)}
+		</ThemeProvider>
+	);
 }
 
 export default App;
